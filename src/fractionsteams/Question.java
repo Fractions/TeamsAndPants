4e651b5051da474f62c7742edbb885feae3bc123
Ben <benrosebery@gmail.com>
2014-11-20 23:33:03 -0500

    Created Qeustion class, reader.

---
+++ src/fractionsteams/Question.java
@@ -0,0 +1,20 @@
+
+package fractionsteams;
+
+import java.io.File;
+
+
+public class Question {
+    
+    int pointValue; // The number of points 
+    int difficulty; // a range of ints, 0 being the easiest
+    String qText; // the actual text of the question
+    String answer; //should I make this an int? Nah. parse based on type of qustion. 
+
+    
+    public Question(){
+        
+    }
+   
+    
+}
